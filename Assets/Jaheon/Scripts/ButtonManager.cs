6d944d22ec77352e3190b5f196d26a129f38780e
H-Jungjaeheon <jjhking222@naver.com>
2022-01-22 19:31:23 +0900

    TitleMake
    TitleMake1

---
+++ Assets/Jaheon/Scripts/ButtonManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] Image Explanation, Setting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Explanation.transform.DOScale(0, 0).SetEase(Ease.InQuad);
+        Setting.transform.DOScale(0, 0).SetEase(Ease.InQuad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+    public void GameStart()
+    {
+        //SceneManager.LoadScene("SunbumScene");
+        SceneManager.LoadScene(2);
+    }
+    public void ExplanationButtonClick()
+    {
+        Explanation.transform.DOScale(1, 0.5f).SetEase(Ease.InQuad);
+    }
+    public void ExplanationExitButtonClick()
+    {
+        Explanation.transform.DOScale(0, 0.5f).SetEase(Ease.InQuad);
+    }
+    public void SettingButtonClick()
+    {
+        Setting.transform.DOScale(1, 0.5f).SetEase(Ease.InQuad);
+    }
+    public void SettingExitButtonClick()
+    {
+        Setting.transform.DOScale(0, 0.5f).SetEase(Ease.InQuad);
+    }
+}
