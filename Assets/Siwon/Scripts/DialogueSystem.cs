a1e0d77a69a0b112cbd52a7fe0867584114573fe
wtsiwon <siwon4727@gmail.com>
2022-01-22 12:14:31 +0900

    다이얼로그

---
+++ Assets/Siwon/Scripts/DialogueSystem.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public Text txtName;
+    public Text txtsentence;
+
+    Queue<string> sentences = new Queue<string>();
+    public Animator anim;
+    public void Begin(Dialogue info)
+    {
+        anim.SetBool("isOpen",true);
+
+        sentences.Clear();
+
+        txtName.text = info.name;
+        foreach (var sentence in info.sentences)
+        {
+            sentences.Enqueue(sentence);
+
+        }
+        Next();
+    }
+    public void Next()
+    {
+        if(sentences.Count == 0)
+        {
+            End();
+            return;
+        }
+        //txtsentence.text = sentences.Dequeue();
+        txtsentence.text = string.Empty;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentences.Dequeue()));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        foreach(var letter in sentence)
+        {
+            txtsentence.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+    private void End()
+    {
+        anim.SetBool("isOpen", false);
+        txtsentence.text = string.Empty;
+    }
+}
